d6d9b6b5b6c5abb2890d996552337be34d807eab
Shotaro Ooki <shoutaro0726@gmail.com>
2020-04-04 10:52:08 +0900

    Commit

--- movie/settings/settings_common.py
+++ movie/settings/settings_common.py
@@ -130,9 +130,9 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'videos/static'),
+)
 
 MEDIA_URL = '/media/'
 
